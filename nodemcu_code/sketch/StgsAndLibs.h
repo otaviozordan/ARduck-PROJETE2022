da6292a1352b422e8970078abf23868c26dbe718
Otávio Zordan Alves <otavio.zordan@outlook.com>
2022-09-21 13:04:42 -0300

    Novas Edições (Atenção)

---
+++ nodemcu_code/sketch/StgsAndLibs.h
@@ -0,0 +1,56 @@
+//Bibliotecas para utilização do Display OLED
+// Inicia o display OLED
+// D3 (GPIO ...) -> SDA
+// D5 (GPIO ...) -> SCL
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//Biblioteca para utilização do ESP8266
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+
+//Dados do WiFi
+const char* ssid = "Teleco";
+const char* password = "09876543";
+
+//Dados de rotas
+String IP = "https://  ****************";
+String SEND_DATA_rote = IP+="/tensao"; //Rota para envio de dados de tensao
+String STATE_route = IP+="/state"; //Rota para recebimento de dados de estado
+
+
+//Botão Send
+const int botao = 2;
+//Led para indicar que o ESP recebeu uma requisição
+const int led = 2;
+
+void conectarWiFI(){
+  //Conecta com o WiFi
+  Serial.print("Conectando a ");
+  Serial.print(ssid);
+  Serial.println("");
+  WiFi.begin(ssid, password); //Conecta com o SSID e senha definidos anteriormente
+  while (WiFi.status() != WL_CONNECTED) { //Espera conexão com o WiFi
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connectado"); 
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); //Exibe IP do ESP8266
+}
+
+void inicializarOLED(){
+  //Inicializa display OLED
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); 
+  }
+  display.display();
+}
--- nodemcu_code/sketch/sketch.ino
+++ nodemcu_code/sketch/sketch.ino
@@ -12,37 +12,7 @@
  *      functioning of our application.
 **/
 
-//Bibliotecas para utilização do Display OLED
-// Inicia o display OLED
-// D3 (GPIO ...) -> SDA
-// D5 (GPIO ...) -> SCL
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-//Biblioteca para utilização do ESP8266
-#include <ESP8266HTTPClient.h>
-#include <ESP8266WiFi.h>
-
-//Dados do WiFi
-const char* ssid = "Teleco";
-const char* password = "09876543";
-
-//Dados de rotas
-String IP = "https://  ****************";
-String SEND_DATA_rote = IP+="/tensao"; //Rota para envio de dados de tensao
-String STATE_route = IP+="/state"; //Rota para recebimento de dados de estado
-
-
-//Botão Send
-const int botao = 2;
-//Led para indicar que o ESP recebeu uma requisição
-const int led = 2;
+#include "StgsAndLibs.h"
 
 void setup() {
   //Botão send para enviar a requisição
@@ -51,30 +21,12 @@ void setup() {
   //Led de indicação
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
-  
+
   //Inicia Monitor Serial para Debug
   Serial.begin(115200);
 
-  //Inicializa display OLED
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); 
-  }
-  display.display();
-
-  //Conecta com o WiFi
-  Serial.print("Conectando a ");
-  Serial.print(ssid);
-  Serial.println("");
-  WiFi.begin(ssid, password); //Conecta com o SSID e senha definidos anteriormente
-  while (WiFi.status() != WL_CONNECTED) { //Espera conexão com o WiFi
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("Connectado"); 
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP()); //Exibe IP do ESP8266
+  inicializarOLED();
+  conectarWiFI();
 }
 
 void loop() {
@@ -88,18 +40,9 @@ void loop() {
     Serial.println(httpCode);
     if(httpCode > 0){
       String payload = http.getString();
-      Serial.println(payload);
-      if (payload){
-        Serial.println("Medição Iniciada");
       }
     }
     http.end();
-  }
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("Sem conexão com a internet");
-    delay(500);
   }
 
   if (/*digitalRead(botao)*/true) //Teste
