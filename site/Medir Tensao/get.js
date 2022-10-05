be44836fc097361154bc2dec20059965351d9bc7
Otávio Zordan Alves <otavio.zordan@outlook.com>
2022-10-04 22:06:00 -0300

    Demo

---
+++ site/Medir Tensao/get.js
@@ -0,0 +1,14 @@
+function fazGet(url) {
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
+
+function get(){
+    event.preventDefault();
+    let url = "https://arduckapi.otaviozordan.repl.co/circuito/" + document.getElementById("id").value
+    let data = fazGet(url)
+    console.log(data)
+    document.getElementById("demo").innerHTML = JSON.stringify((data), null, 4);
+}
